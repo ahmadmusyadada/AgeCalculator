ed6ec59314e19022c93debe77d823b545fac2f1a
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-10-12 10:45:36 +0700

    Add more detail for age

--- app/src/main/java/id/ac/polinema/uts/fragments/BirthdayFragment.java
+++ app/src/main/java/id/ac/polinema/uts/fragments/BirthdayFragment.java
@@ -3,14 +3,22 @@ package id.ac.polinema.uts.fragments;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -22,7 +30,10 @@ import id.ac.polinema.uts.R;
  * A simple {@link Fragment} subclass.
  */
 public class BirthdayFragment extends Fragment {
-    TextView currentDateDisplay, dateBirthday;
+    TextView currentDate, dateBirthday,
+            currentBirthdayDay, currentBirthdayMonth, currentBirthdayYear,
+            nextBirthdayDay, nextBirthdayMonth, nextBirthdayYear;
+    Button calculateButton;
 
     Calendar calendar;
     DatePickerDialog dpd;
@@ -38,15 +49,22 @@ public class BirthdayFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_birthday, container, false);
 
-        currentDateDisplay = view.findViewById(R.id.current_date);
+        currentDate = view.findViewById(R.id.current_date);
         dateBirthday = view.findViewById(R.id.birthday_date);
+        calculateButton = view.findViewById(R.id.button_calculate);
+        currentBirthdayDay = view.findViewById(R.id.days_in_age);
+        currentBirthdayMonth = view.findViewById(R.id.months_in_age);
+        currentBirthdayYear = view.findViewById(R.id.years_in_age);
+        nextBirthdayDay = view.findViewById(R.id.days_in_birthday);
+        nextBirthdayMonth = view.findViewById(R.id.months_in_birthday);
+        nextBirthdayYear = view.findViewById(R.id.years_in_birthday);
 
         Date c = Calendar.getInstance().getTime();
         SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         String formattedDate = df.format(c);
-        currentDateDisplay.setText(formattedDate);
+        currentDate.setText(formattedDate);
 
-        currentDateDisplay.setOnClickListener(new View.OnClickListener() {
+        currentDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 calendar = Calendar.getInstance();
@@ -57,7 +75,7 @@ public class BirthdayFragment extends Fragment {
                 dpd = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int mYear, int mMonth, int mDay) {
-                        currentDateDisplay.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
+                        currentDate.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
                     }
                 }, year, month, day);
                 dpd.show();
@@ -81,6 +99,64 @@ public class BirthdayFragment extends Fragment {
                 dpd.show();
             }
         });
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getTodayDate = currentDate.getText().toString();
+                String getBirthdayDate = dateBirthday.getText().toString();
+                DateTime todayDateTime = convertToDateTime(getTodayDate);
+                DateTime birthdayDateTime = convertToDateTime(getBirthdayDate);
+                displayCurrentBirthday(todayDateTime, birthdayDateTime);
+//                displayNextBirthday(todayDateTime, birthdayDateTime);
+//                displayAgeAnalysis(todayDateTime, birthdayDateTime);
+            }
+        });
         return view;
     }
+
+    private DateTime convertToDateTime(String stringToConvert) {
+        String[] newStringArray = convertStringToArray(stringToConvert);
+        int year = Integer.parseInt(newStringArray[2].trim());
+        int day = Integer.parseInt(newStringArray[1].trim());
+        int month = Integer.parseInt(newStringArray[0].trim());
+        LocalDate mLocalDate = new LocalDate(year, month, day);
+        DateTime firstDateTime = mLocalDate.toDateTime(LocalTime.fromDateFields(mLocalDate.toDate()));
+        return firstDateTime;
+    }
+
+    private String[] convertStringToArray(String stringToConvert){
+        String[] newStringArray = stringToConvert.split("/");
+        return newStringArray;
+    }
+
+    private void displayCurrentBirthday(DateTime dateToday, DateTime birthdayDate){
+        Period dateDifferencePeriod = displayBirthdayResult(dateToday, birthdayDate);
+        int getDateInDays = dateDifferencePeriod.getDays();
+        int getDateInMonths = dateDifferencePeriod.getMonths();
+        int getDateInYears = dateDifferencePeriod.getYears();
+        currentBirthdayDay.setText(Html.fromHtml("<h4>Days</h4>" + getDateInDays));
+        currentBirthdayMonth.setText(Html.fromHtml("<h4>Months</h4>" + getDateInMonths));
+        currentBirthdayYear.setText(Html.fromHtml("<h4>Years</h4>" + getDateInYears));
+    }
+
+    private void displayNextBirthday(DateTime dateToday, DateTime birthdayDate){
+        Calendar mCalendar = Calendar.getInstance();
+        int year = mCalendar.get(Calendar.YEAR);
+        DateTime nextBirthday = birthdayDate.withYear(year);
+        Toast.makeText(getActivity(), "Birthday " + nextBirthday.getYear(), Toast.LENGTH_LONG).show();
+        Period dateDifferencePeriod = displayBirthdayResult(nextBirthday, dateToday);
+        int getDateInDays = dateDifferencePeriod.getDays();
+        int getDateInMonths = dateDifferencePeriod.getMonths();
+        int getDateInYears = dateDifferencePeriod.getYears();
+        nextBirthdayDay.setText(Html.fromHtml("<h4>Days</h4>" + getDateInDays));
+        nextBirthdayMonth.setText(Html.fromHtml("<h4>Months</h4>" + getDateInMonths));
+        nextBirthdayYear.setText(Html.fromHtml("<h4>Years</h4>" + getDateInYears));
+    }
+
+    private Period displayBirthdayResult(DateTime dateToday, DateTime birthdayDate){
+        Period dateDifferencePeriod = new Period(birthdayDate, dateToday, PeriodType.yearMonthDayTime());
+        return dateDifferencePeriod;
+    }
+
 }
