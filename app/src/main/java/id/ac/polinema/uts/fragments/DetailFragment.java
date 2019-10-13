0c928f76a1c8e58d1c5d0dc45ce9d26a6af9d040
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-10-13 09:37:51 +0700

    Layout Detail Fragment

--- app/src/main/java/id/ac/polinema/uts/fragments/DetailFragment.java
+++ app/src/main/java/id/ac/polinema/uts/fragments/DetailFragment.java
@@ -1,13 +1,22 @@
 package id.ac.polinema.uts.fragments;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
+import java.util.Calendar;
 
 import id.ac.polinema.uts.R;
 
@@ -15,7 +24,10 @@ import id.ac.polinema.uts.R;
  * A simple {@link Fragment} subclass.
  */
 public class DetailFragment extends Fragment {
-
+    Button calculateButton;
+    TextView dateSelected;
+    Calendar calendar;
+    DatePickerDialog dpd;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -26,7 +38,54 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        calculateButton = view.findViewById(R.id.button_calculate_detail);
+        dateSelected = view.findViewById(R.id.display_date);
+
+        dateSelected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int mYear, int mMonth, int mDay) {
+                        dateSelected.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
+                    }
+                }, year, month, day);
+                dpd.show();
+            }
+        });
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getTodayDate = dateSelected.getText().toString();
+                DateTime todayDateTime = convertToDateTime(getTodayDate);
+                displayCurrentBirthday(todayDateTime);
+            }
+        });
+        return view;
     }
 
+    private String[] convertStringToArray(String stringToConvert){
+        String[] newStringArray = stringToConvert.split("/");
+        return newStringArray;
+    }
+
+    private DateTime convertToDateTime(String stringToConvert) {
+        String[] newStringArray = convertStringToArray(stringToConvert);
+        int year = Integer.parseInt(newStringArray[2].trim());
+        int day = Integer.parseInt(newStringArray[0].trim());
+        int month = Integer.parseInt(newStringArray[1].trim());
+        LocalDate mLocalDate = new LocalDate(year, month, day);
+        DateTime firstDateTime = mLocalDate.toDateTime(LocalTime.fromDateFields(mLocalDate.toDate()));
+        return firstDateTime;
+    }
+
+    private void displayCurrentBirthday(DateTime dateToday){
+    }
 }
