9d0266dafbc92f3916fd9f7e083984d89d895934
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-10-13 14:40:52 +0700

    Fix birthday detail

--- app/src/main/java/id/ac/polinema/uts/fragments/DetailFragment.java
+++ app/src/main/java/id/ac/polinema/uts/fragments/DetailFragment.java
@@ -3,6 +3,7 @@ package id.ac.polinema.uts.fragments;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +16,12 @@ import androidx.fragment.app.Fragment;
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import id.ac.polinema.uts.R;
 
@@ -24,8 +29,9 @@ import id.ac.polinema.uts.R;
  * A simple {@link Fragment} subclass.
  */
 public class DetailFragment extends Fragment {
+    TextView dateSelected, totalDayResult, totalWeekResult, totalMonthResult, totalYearResult,
+            totalHourResult, totalMinuteResult, totalSecondResult, zodiacResult;
     Button calculateButton;
-    TextView dateSelected;
     Calendar calendar;
     DatePickerDialog dpd;
 
@@ -40,7 +46,15 @@ public class DetailFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
         calculateButton = view.findViewById(R.id.button_calculate_detail);
-        dateSelected = view.findViewById(R.id.display_date);
+        dateSelected = view.findViewById(R.id.birthday_date);
+        totalYearResult = view.findViewById(R.id.total_years_result);
+        totalMonthResult = view.findViewById(R.id.total_months_result);
+        totalWeekResult = view.findViewById(R.id.total_weeks_result);
+        totalDayResult = view.findViewById(R.id.total_days_result);
+        totalHourResult = view.findViewById(R.id.total_hours_result);
+        totalMinuteResult = view.findViewById(R.id.total_minutes_result);
+        totalSecondResult = view.findViewById(R.id.total_seconds_result);
+        zodiacResult = view.findViewById(R.id.zodiac_result);
 
         dateSelected.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,11 +78,36 @@ public class DetailFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 String getTodayDate = dateSelected.getText().toString();
-                DateTime todayDateTime = convertToDateTime(getTodayDate);
-                displayCurrentBirthday(todayDateTime);
+                DateTime birthdayDate = convertToDateTime(getTodayDate);
+                displayAgeAnalysis(birthdayDate);
             }
         });
         return view;
+    }
+
+    private void displayAgeAnalysis(DateTime birthdayDate) {
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String dateToday = df.format(c);
+        DateTime todayDateTime = convertToDateTime(dateToday);
+        Period dateDifferencePeriod = displayBirthdayResult(todayDateTime, birthdayDate);
+        int getDateInDays = dateDifferencePeriod.getDays();
+        float getDateInWeeks = (float) getDateInDays / 7;
+        int getDateInMonths = dateDifferencePeriod.getMonths();
+        int getDateInYears = dateDifferencePeriod.getYears();
+        int mDay = getDateInDays;
+        int mMonth = getDateInMonths + (getDateInYears * 12);
+        int hours = mDay * 24;
+        int minutes = mDay * 24 * 60;
+        int seconds = mDay * 24 * 60 * 60;
+        totalYearResult.setText(Html.fromHtml(String.valueOf(getDateInYears)));
+        totalMonthResult.setText(Html.fromHtml(String.valueOf(mMonth)));
+        totalWeekResult.setText(Html.fromHtml(String.valueOf(getDateInWeeks)));
+        totalDayResult.setText(Html.fromHtml(String.valueOf(mDay)));
+        totalHourResult.setText("±" + Html.fromHtml(String.valueOf(hours)));
+        totalMinuteResult.setText("±" + Html.fromHtml(String.valueOf(minutes)));
+        totalSecondResult.setText("±" + Html.fromHtml(String.valueOf(seconds)));
+        zodiacResult.setText(Html.fromHtml(displayZodiacResult(birthdayDate)));
     }
 
     private String[] convertStringToArray(String stringToConvert){
@@ -86,6 +125,99 @@ public class DetailFragment extends Fragment {
         return firstDateTime;
     }
 
-    private void displayCurrentBirthday(DateTime dateToday){
+    private Period displayBirthdayResult(DateTime dateToday, DateTime birthdayDate){
+        Period dateDifferencePeriod = new Period(birthdayDate, dateToday, PeriodType.yearMonthDayTime());
+        return dateDifferencePeriod;
+    }
+
+    private String displayZodiacResult(DateTime birthdayDate){
+        String astro_sign="";
+        int month = birthdayDate.getMonthOfYear();
+        int day = birthdayDate.getDayOfMonth();
+        // checks month and date within the
+        // valid range of a specified zodiac
+        if (month == 12){
+            if (day < 22)
+                astro_sign = "Sagittarius";
+            else
+                astro_sign ="capricorn";
+        }
+
+        else if (month == 1){
+            if (day < 20)
+                astro_sign = "Capricorn";
+            else
+                astro_sign = "aquarius";
+        }
+
+        else if (month == 2){
+            if (day < 19)
+                astro_sign = "Aquarius";
+            else
+                astro_sign = "pisces";
+        }
+
+        else if(month == 3){
+            if (day < 21)
+                astro_sign = "Pisces";
+            else
+                astro_sign = "aries";
+        }
+        else if (month == 4){
+            if (day < 20)
+                astro_sign = "Aries";
+            else
+                astro_sign = "taurus";
+        }
+
+        else if (month == 5){
+            if (day < 21)
+                astro_sign = "Taurus";
+            else
+                astro_sign = "gemini";
+        }
+
+        else if( month == 6){
+            if (day < 21)
+                astro_sign = "Gemini";
+            else
+                astro_sign = "cancer";
+        }
+
+        else if (month == 7){
+            if (day < 23)
+                astro_sign = "Cancer";
+            else
+                astro_sign = "leo";
+        }
+
+        else if( month == 8){
+            if (day < 23)
+                astro_sign = "Leo";
+            else
+                astro_sign = "virgo";
+        }
+
+        else if (month == 9){
+            if (day < 23)
+                astro_sign = "Virgo";
+            else
+                astro_sign = "libra";
+        }
+
+        else if (month == 10){
+            if (day < 23)
+                astro_sign = "Libra";
+            else
+                astro_sign = "scorpio";
+        }
+
+        else if (month == 11){
+            if (day < 22)
+                astro_sign = "scorpio";
+            else
+                astro_sign = "sagittarius";
+        }
+        return astro_sign;
     }
 }
